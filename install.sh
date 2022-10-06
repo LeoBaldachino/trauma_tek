332a56e5c349155b4db797332be8b99b20f87c6c
david <david.benistant@epitech.eu>
2022-10-06 22:36:49 +0200

    ui

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ cd $HOME
 mkdir ".cat"
 cd ".cat"
 wget 'https://github.com/LeoBaldachino/trauma_tek/raw/main/nyanCat.wav'
-wget 'https://raw.githubusercontent.com/LeoBaldachino/trauma_tek/aaa26397b91e8a0fe6e29d2e76b8c8e6880a4f92/WTF_am_I_doing.c'
+wget 'https://raw.githubusercontent.com/LeoBaldachino/trauma_tek/main/WTF_am_I_doing.c'
 gcc WTF_am_I_doing.c -lcsfml-audio
+rm WTF_am_I_doing.c
 ./a.out
