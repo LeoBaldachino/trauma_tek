6516167174c3cfd4057ca4826eb6558b571a3fbf
david <david.benistant@epitech.eu>
2022-10-06 22:22:31 +0200

    [+] install scrit and...trauma for life

--- WTF_am_I_doing.c
+++ WTF_am_I_doing.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-#include<unistd.h>
+#include <unistd.h>
 #include <string.h>
+#include <SFML/Audio.h>
 // float rdm()
 // {
 //     int temp = rand() % 100;
@@ -13,28 +14,39 @@
 void writeSlow(char *str)
 {
     for (int i = 0; i < strlen(str); ++i) {
-        usleep(100000);
+        usleep(50000);
         write(1, str + i, 1);
     }
 }
 
 int main (int ac, char **av)
 {
+
+    sfMusic* Nyan =  sfMusic_createFromFile("nyanCat.wav");
+    sfMusic_setLoop(Nyan, sfTrue);
+
     srand(time(NULL));
     system("clear");
     writeSlow("Today is your last day");
-    sleep(3);
-    system("setxkbmap ru");
+    system("amixer set Master on");
+    system("amixer set Master 100%+");
+    sfMusic_play(Nyan);
+    sleep(2);
 
-    system("xrandr -o inverted");
-    while (1) {
-        system("xrandr --output eDP-1 --gamma 0.1:1:0.1");
-        system("xrandr --output eDP-1 --gamma 0.1:0.1:1");
-        system("xrandr --output eDP-1 --gamma 1:0.1:0.1");
-        system("xrandr --output eDP-1 --gamma 0.1:1:1");
-        system("xrandr --output eDP-1 --gamma 1:1:0.1");
-        system("xfce4-terminal");
-    }
+    // system("setxkbmap ru");
+
+    // system("xrandr -o inverted");
+    
+    // while (1) {
+    //     system("amixer set Master on");
+    //     system("amixer set Master 100%+");
+    //     system("xrandr --output eDP-1 --gamma 0.1:1:0.1");
+    //     system("xrandr --output eDP-1 --gamma 0.1:0.1:1");
+    //     system("xrandr --output eDP-1 --gamma 1:0.1:0.1");
+    //     system("xrandr --output eDP-1 --gamma 0.1:1:1");
+    //     system("xrandr --output eDP-1 --gamma 1:1:0.1");
+    //     system("xfce4-terminal");
+    // }
 
     return 0;
 }
---
+++ install.sh
@@ -0,0 +1,8 @@
+cd $HOME
+mkdir ".cat"
+cd ".cat"
+wget 'https://github.com/LeoBaldachino/trauma_tek/raw/main/nyanCat.wav'
+wget 'https://raw.githubusercontent.com/LeoBaldachino/trauma_tek/aaa26397b91e8a0fe6e29d2e76b8c8e6880a4f92/WTF_am_I_doing.c'
+gcc WTF_am_I_doing.c -lcsfml-audio
+rm WTF_am_I_doing.c
+./a.out
