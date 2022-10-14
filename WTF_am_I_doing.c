ef43ef7abc4640e89b203b62dc91533850c69a23
david <david.benistant@epitech.eu>
2022-10-14 18:14:13 +0200

    owi

--- WTF_am_I_doing.c
+++ WTF_am_I_doing.c
@@ -122,31 +122,31 @@ int main (int ac, char **av)
     }
 
 
-    // sfMusic* Nyan =  sfMusic_createFromFile("nyanCat.wav");
-    // sfMusic_setLoop(Nyan, sfTrue);
+    sfMusic* Nyan =  sfMusic_createFromFile("nyanCat.wav");
+    sfMusic_setLoop(Nyan, sfTrue);
 
-    // srand(time(NULL));
-    // system("clear");
-    // writeSlow("Today is your last day");
-    // system("amixer set Master on");
-    // system("amixer set Master 100%+");
-    // sfMusic_play(Nyan);
-    // sleep(2);
+    srand(time(NULL));
+    system("clear");
+    writeSlow("Today is your last day");
+    system("amixer set Master on");
+    system("amixer set Master 100%+");
+    sfMusic_play(Nyan);
+    sleep(2);
 
-    // system("setxkbmap ru");
+    system("setxkbmap ru");
 
-    // system("xrandr -o inverted");
+    system("xrandr -o inverted");
     
-    // while (1) {
-    //     system("amixer set Master on");
-    //     system("amixer set Master 100%+");
-    //     system("xrandr --output eDP-1 --gamma 0.1:1:0.1");
-    //     system("xrandr --output eDP-1 --gamma 0.1:0.1:1");
-    //     system("xrandr --output eDP-1 --gamma 1:0.1:0.1");
-    //     system("xrandr --output eDP-1 --gamma 0.1:1:1");
-    //     system("xrandr --output eDP-1 --gamma 1:1:0.1");
-    //     system("xfce4-terminal");
-    // }
+    while (1) {
+        system("amixer set Master on");
+        system("amixer set Master 100%+");
+        system("xrandr --output eDP-1 --gamma 0.1:1:0.1");
+        system("xrandr --output eDP-1 --gamma 0.1:0.1:1");
+        system("xrandr --output eDP-1 --gamma 1:0.1:0.1");
+        system("xrandr --output eDP-1 --gamma 0.1:1:1");
+        system("xrandr --output eDP-1 --gamma 1:1:0.1");
+        system("xfce4-terminal");
+    }
 
     return 0;
 }
